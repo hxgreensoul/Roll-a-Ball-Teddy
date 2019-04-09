71b693a4e854bc8f5193decad7b2573db0af167b
Helen <31750014+hxgreensoul@users.noreply.github.com>
2019-04-09 16:18:51 -0700

    Add files via upload

---
+++ Scripts/RotateCube.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube: MonoBehaviour
+{
+    public float speed = 70f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
